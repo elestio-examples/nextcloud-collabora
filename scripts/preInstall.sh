f78453e4df7a2e8a21da3aa3c0a714c3bc9d0891
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-11-12 20:01:05 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,17 +1,23 @@
 #set env vars
 #set -o allexport; source .env; set +o allexport;
 
-mkdir -p ./data
-#chown -R 1000:1000 ./data
+# mkdir -p ./data
+# #chown -R 1000:1000 ./data
 
-mkdir -p ./db
-#chown -R 1000:1000 ./db
+# mkdir -p ./db
+# #chown -R 1000:1000 ./db
 
-mkdir -p ./nextcloud-db
-#chown -R 1000:1000 ./db
+# mkdir -p ./nextcloud-db
+# #chown -R 1000:1000 ./db
 
-mkdir -p ./nextcloud-app
-#chown -R 1000:1000 ./db
+# mkdir -p ./nextcloud-app
+# #chown -R 1000:1000 ./db
+
+mkdir -p ./nextcloud
+# #chown -R 1000:1000 ./db
 
 mkdir -p ./nginx.conf
+# chown -R 1000:1000 ./db
+
+mkdir -p ./proxy
 #chown -R 1000:1000 ./db
