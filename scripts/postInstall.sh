f0e5612a55e05d613404c26c66c7303102044cda
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-11-13 13:57:59 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,48 +4,8 @@ set -o allexport; source .env; set +o allexport;
 sleep 20s;
 
 
-# utiliser sed dans nginx pour configurer
-
-# chemin
-
-# /opt/elestio/nginx/conf.d/${DOMAIN}.conf
-
-# utiliser sed insert after listen 21005 ssl http2;
-
 sed -i 's@listen 21005 ssl http2;@listen 21005 ssl http2;\nlocation ^~ /browser {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Host $http_host;\n}\n\n# main websocket\nlocation ~ ^/cool/(.*)/ws$ {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Upgrade $http_upgrade;\nproxy_set_header Connection "Upgrade";\nproxy_set_header Host $http_host;\nproxy_read_timeout 36000s;\n}\n\n# download, presentation and image upload\nlocation ~ ^/(c|l)ool {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Host $http_host;\n}\n\n# Admin Console websocket\nlocation ^~ /cool/adminws {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Upgrade $http_upgrade;\nproxy_set_header Connection "Upgrade";\nproxy_set_header Host $http_host;\nproxy_read_timeout 36000s;\n}\n@g' /opt/elestio/nginx/conf.d/${DOMAIN}.conf
 
-# 1.
-
-# location ^~ /browser {
-#         proxy_pass http://172.17.0.1:21002;
-#         proxy_set_header Host $http_host;
-#     }
-
-#    # main websocket
-#    location ~ ^/cool/(.*)/ws$ {
-#        proxy_pass http://172.17.0.1:21002;
-#        proxy_set_header Upgrade $http_upgrade;
-#        proxy_set_header Connection "Upgrade";
-#        proxy_set_header Host $http_host;
-#        proxy_read_timeout 36000s;
-#    }
-
-#    # download, presentation and image upload
-#    location ~ ^/(c|l)ool {
-#        proxy_pass http://172.17.0.1:21002;
-#        proxy_set_header Host $http_host;
-#    }
-
-#    # Admin Console websocket
-#    location ^~ /cool/adminws {
-#        proxy_pass http://172.17.0.1:21002;
-#        proxy_set_header Upgrade $http_upgrade;
-#        proxy_set_header Connection "Upgrade";
-#        proxy_set_header Host $http_host;
-#        proxy_read_timeout 36000s;
-#    }
-
-# restart nginx
 
 docker exec elestio-nginx nginx -s reload;
 
