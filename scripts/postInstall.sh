b5d681cace3f2ada8b680a8757d127f2a13b1da0
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-11-13 12:52:43 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -12,7 +12,7 @@ sleep 20s;
 
 # utiliser sed insert after listen 21005 ssl http2;
 
-sed -i 's@listen 21005 ssl http2;@listen 21005 ssl http2;\nlocation ^~ /browser {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Host $http_host;\n}\n\n# main websocket\nlocation ~ ^/cool/(.*)/ws$ {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Upgrade $http_upgrade;\nproxy_set_header Connection "Upgrade";\nproxy_set_header Host $http_host;\nproxy_read_timeout 36000s;\n}\n\n# download, presentation and image upload\nlocation ~ ^/(c|l)ool {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Host $http_host;\n}\n\n# Admin Console websocket\nlocation ^~ /cool/adminws {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Upgrade $http_upgrade;\nproxy_set_header Connection "Upgrade";\nproxy_set_header Host $http_host;\nproxy_read_timeout 36000s;\n}\n@g' /opt/elestio/nginx/conf.d/nextcloud-collabora-urbosa-u353.vm.elestio.app.conf
+sed -i 's@listen 21005 ssl http2;@listen 21005 ssl http2;\nlocation ^~ /browser {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Host $http_host;\n}\n\n# main websocket\nlocation ~ ^/cool/(.*)/ws$ {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Upgrade $http_upgrade;\nproxy_set_header Connection "Upgrade";\nproxy_set_header Host $http_host;\nproxy_read_timeout 36000s;\n}\n\n# download, presentation and image upload\nlocation ~ ^/(c|l)ool {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Host $http_host;\n}\n\n# Admin Console websocket\nlocation ^~ /cool/adminws {\nproxy_pass http://172.17.0.1:21002;\nproxy_set_header Upgrade $http_upgrade;\nproxy_set_header Connection "Upgrade";\nproxy_set_header Host $http_host;\nproxy_read_timeout 36000s;\n}\n@g' /opt/elestio/nginx/conf.d/${DOMAIN}.conf
 
 # 1.
 
@@ -67,4 +67,13 @@ curl http://${target}/index.php \
   sed -i "s|'overwrite.cli.url' => 'https://172.17.0.1:21000'|'overwrite.cli.url' => 'https://"${DOMAIN}"'|g" ./config/config.php
 
   sed -i "s|'installed' => true,|'installed' => true,\n  'mail_from_address' => '"$MAIL_FROM_ADDRESS"',\n  'mail_smtpmode' => 'smtp',\n  'mail_sendmailmode' => 'smtp',\n  'mail_domain' => '"$MAIL_DOMAIN"',\n  'mail_smtpport' => '"$EMAIL_PORT"',\n  'mail_smtphost' => '"$EMAIL_HOST"',|g" ./config/config.php
+
+  curl http://${target}/ocs/v2.php/cloud/users/root \
+  -X 'PUT' \
+  -H 'accept: application/json, text/plain, */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'content-type: application/json' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36' \
+  --data-raw '{"key":"email","value":"'${ADMIN_EMAIL}'"}' \
+  --compressed
 
